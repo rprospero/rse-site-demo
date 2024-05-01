6ab88c0c53ef6c5a797ea54d2305d98e1a17fbb5
Adam Washington <adam.washington@stfc.ac.uk>
2024-05-01 18:10:45 +0100

    Proper MDANSE version checking

--- sites.py
+++ sites.py
@@ -22,6 +22,18 @@ class SiteChecks(unittest.TestCase):
         actual = soup.select_one('a[href="#release"]').get_text()
         self.assertEqual(version, actual)
 
+    def test_mdanse(self):
+        import re
+        version = "1.5.2"
+
+        r = requests.get("https://www.isis.stfc.ac.uk/Pages/MDANSEproject.aspx")
+        self.assertEqual(r.status_code, 200)
+        soup = BeautifulSoup(r.text, features="html.parser")
+        result = [re.search("version (.....)", line.get_text().strip()).group(1)
+                  for line in soup.select('p')
+                  if "Download latest" in line.get_text().strip()][0]
+        self.assertEqual(version, result)
+
     def test_sasview(self):
         version = "v5.0.6"
 
@@ -33,7 +45,6 @@ class SiteChecks(unittest.TestCase):
         self.assertEqual(version, actual)
 
     def test_sscanss(self):
-        import re
         version = "v2.1.1"
 
         r = requests.get("https://github.com/ISISNeutronMuon/SScanSS-2/releases")
